9fc23277ec5b508b94c7baf8d60fef86e1bb4f67
alphine89 <115539576+alphine89@users.noreply.github.com>
2022-10-24 00:40:42 -0800

    Create spinning.py

---
+++ spinning.py
@@ -0,0 +1,16 @@
+import turtle
+turtle. speed(0)
+turtle. bgcolor("black") 
+for i in range (15) :
+    for colours in ("red", "magneta" "blue"  "orange" "yellow "purple" "cyam" "grey" )
+    turtle. color(colours)
+    turtle. pensize(4)
+    turtle. left(5)
+    turtle. forward(200)
+    turtle. left(90)
+    turtle. forward(200)
+    turtle. left(90)
+    turtle. forward(200)
+    turtle. left(90)
+    turtle. forward(200)
+    turtle. left(90)
